db9bba57e8c0f0d024bd2f47841c02c6b4dcfb0f
wahahabao <1194220986@qq.com>
2017-10-24 13:29:02 +0800

    分享代码到github上

---
+++ app/src/main/java/com/wonongyun/awl/awl_wonongyun/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wonongyun.awl.awl_wonongyun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
