ba1762d42466f8d05b420c1c3b5ce4cbdffad973
wangxiufeng <1194220986@qq.com>
2017-10-24 17:18:51 +0800

    测试butter_knife

--- app/src/main/java/com/wonongyun/awl/awl_wonongyun/MainActivity.java
+++ app/src/main/java/com/wonongyun/awl/awl_wonongyun/MainActivity.java
@@ -1,16 +1,38 @@
 package com.wonongyun.awl.awl_wonongyun;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    @Bind(R.id.btn_count)
+    Button btn_count;
+    @Bind(R.id.tv_showCount)
+    TextView tv_showCount;
+    private int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("测试commit and push","点击一下");
-        Log.i("测试user.name email","点击一下");
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_count, R.id.tv_showCount})
+    public void onViewClicked(View view) {
+        int id = view.getId();
+        if (id == R.id.btn_count) {
+//            tv_showCount.setText("次数："+i);
+            tv_showCount.setText(i);
+            i++;
+        } else if (id == R.id.tv_showCount) {
+            Toast.makeText(this,"textview",Toast.LENGTH_SHORT).show();
+        }
     }
 }
