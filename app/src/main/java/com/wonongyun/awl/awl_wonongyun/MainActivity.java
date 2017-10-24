fa37961ef3a7f02ab30cb15fe34d4218d0f249a2
wahahabao <1194220986@qq.com>
2017-10-24 13:37:30 +0800

    测试本地提交

--- app/src/main/java/com/wonongyun/awl/awl_wonongyun/MainActivity.java
+++ app/src/main/java/com/wonongyun/awl/awl_wonongyun/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("测试commit and push","点击一下");
+        Log.i("测试本地提交","点击一下");
     }
 }
