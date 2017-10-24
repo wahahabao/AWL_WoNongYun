81e0a550017dc08d31a48b8d912b48977e558f3b
wahahabao <1194220986@qq.com>
2017-10-24 13:34:05 +0800

    测试commit and push

--- app/src/main/java/com/wonongyun/awl/awl_wonongyun/MainActivity.java
+++ app/src/main/java/com/wonongyun/awl/awl_wonongyun/MainActivity.java
@@ -2,6 +2,7 @@ package com.wonongyun.awl.awl_wonongyun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("测试commit and push","点击一下");
     }
 }
