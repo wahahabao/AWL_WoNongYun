b15d18008913e79d5ca66efe26ae3bce3d0a4027
wangxiufeng <1194220986@qq.com>
2017-10-30 11:00:36 +0800

    新建了TestDatabinding分支

--- app/src/main/java/com/wonongyun/awl/awl_wonongyun/databinding/TestAgainActivity.java
+++ app/src/main/java/com/wonongyun/awl/awl_wonongyun/databinding/TestAgainActivity.java
@@ -30,7 +30,7 @@ public class TestAgainActivity extends Activity {
         binding.setWxfOnClickL(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("TAG","测试成功");
+                Log.i("TAG"," 测试成功");
             }
         });
     }
